67d24f99dde2f1e256a6ff336d6266c522842eef
Sayan7211 <96005220+Sayan7211@users.noreply.github.com>
2022-01-17 15:37:21 -0500

    Create remove.py

---
+++ remove.py
@@ -0,0 +1,6 @@
+import os
+n=input("Enter Your File Name Which You Want To Remove:")
+os.remove(n)
+print("\n{0} This File Is Succesfully Remove.\n".format(n))
+print("\t\t\t\t\t _______MAKE BY SAYAN")
+
