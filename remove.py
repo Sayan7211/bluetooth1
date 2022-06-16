7cc1b275285d44a9406042ce9b0c4124b24c17d4
Sayan7211 <96005220+Sayan7211@users.noreply.github.com>
2022-06-16 11:48:54 -0400

    Delete remove.py

--- remove.py
+++
@@ -1,6 +0,0 @@
-import os
-n=input("Enter Your File Name Which You Want To Remove:")
-os.remove(n)
-print("\n{0} This File Is Succesfully Remove.\n".format(n))
-print("\033[5;37;48m\t\t\t\t\t__________Sayan Das\033[0m")
-
